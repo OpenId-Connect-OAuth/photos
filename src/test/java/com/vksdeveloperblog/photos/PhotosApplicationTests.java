9d1d43b82b7311ec3e6e898f34a7ac7df7c47b1c
Vikkas Taneja <vikkastaneja@users.noreply.github.com>
2024-11-12 10:06:57 -0800

    Add files via upload

---
+++ src/test/java/com/vksdeveloperblog/photos/PhotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vksdeveloperblog.photos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
